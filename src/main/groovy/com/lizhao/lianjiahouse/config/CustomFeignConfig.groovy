9274c0b69b3e19a8472aba2b83e56682bf94e0b4
lizhaode <308697716@qq.com>
2019-09-12 17:51:51 +0800

    add okhttp log interceptor

--- src/main/groovy/com/lizhao/lianjiahouse/config/CustomFeignConfig.groovy
+++ src/main/groovy/com/lizhao/lianjiahouse/config/CustomFeignConfig.groovy
@@ -1,12 +1,29 @@
 package com.lizhao.lianjiahouse.config
 
 import feign.Logger
+import feign.okhttp.OkHttpClient
+import groovy.util.logging.Slf4j
+import okhttp3.logging.HttpLoggingInterceptor
 import org.springframework.context.annotation.Bean
 
+@Slf4j
 class CustomFeignConfig {
 
     @Bean
     Logger.Level feignLoggerLevel() {
         return Logger.Level.FULL
+    }
+
+    @Bean
+    OkHttpClient feignClientConfig() {
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor(new HttpLoggingInterceptor.Logger() {
+            @Override
+            void log(String s) {
+                log.info(s)
+            }
+        })
+        interceptor.level(HttpLoggingInterceptor.Level.BODY)
+        okhttp3.OkHttpClient client = new okhttp3.OkHttpClient.Builder().addInterceptor(interceptor).build()
+        return new OkHttpClient(client)
     }
 }
